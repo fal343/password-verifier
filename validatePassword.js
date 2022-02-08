8f7ebb5380b40449c5da519787debeda9fefcd98
fal343 <98967311+fal343@users.noreply.github.com>
2022-02-08 11:59:01 +0000

    Update validatePassword.js
    Write answer for special consideration

--- validatePassword.js
+++ validatePassword.js
@@ -1,3 +1,9 @@
+`
+In Regards to the extra consideration:
+Assuming this means that running each check individually takes 1 second, I could run both checks at once by using a regex that evaluates both conditions.
+For example, if running checks 3 and 4 take one second each, I could merge the regexes I'm using into regex groups to evaluate both conditions in a single regex.
+`
+
 function validatePassword(password) {
     let unmetConditionsArray = []
     let isPasswordValid;
