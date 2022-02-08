a38d5623abdc317772efc61e80ec1b8b54422a04
fal343 <98967311+fal343@users.noreply.github.com>
2022-02-08 11:04:24 +0000

    update tests to test new returnObject which contains an array of condition violations and wether the password is valid or not

--- test/index.test.js
+++ test/index.test.js
@@ -7,24 +7,24 @@ describe('Test Jest', () => {
 })
 
 describe('Test validatePassword', () => {
-    test('should return a string if invalid', () => {
-        expect(typeof validatePassword('H3t3')).toBe('string')
+    test('Should return a object if invalid', () => {
+        expect(typeof validatePassword('H3t3')).toBe('object')
     })
     test('It should return invalid if password is less than eight characters', () => {
-        expect(validatePassword('GC$5D')).toBe('Invalid password: Must be longer than eight characters')
+        expect(validatePassword('GC$5D').unmetConditionsArray.includes('Invalid password: Must be longer than eight characters')).toBe(true)
     })
     test('Password should have at least one uppercase Letter', () => {
-        expect(validatePassword('mdkejs23ds')).toBe('Invalid password: Must have at least one uppercase letter')
+        expect(validatePassword('mdkejs23ds').unmetConditionsArray.includes('Invalid password: Must have at least one uppercase letter')).toBe(true)
     })
-    test('Password should have one lowercase letter at least', () => {
-        expect(validatePassword('MDKEDFJS$12')).toBe('Invalid password: Must have at least one lowercase letter')
+    test('Password should have one lowercase letter at least', () => { 
+        expect(validatePassword('MDKEDFJS$12').unmetConditionsArray.includes('Invalid password: Must have at least one lowercase letter')).toBe(true)
     })
     test('Password should have at least one number', () => {
-        expect(validatePassword('DFERdef$qw')).toBe('Invalid password: Must have at least one number')
+        expect(validatePassword('DFERdef$qw').unmetConditionsArray.includes('Invalid password: Must have at least one number')).toBe(true)
     })
     test('Valid passwords should be accepted', () => {
-        expect(validatePassword('D152ed1f$qw')).toBe(true)
-        expect(validatePassword('Plcd$edq234d')).toBe(true)
+        expect(validatePassword('D152ed1f$qw').isPasswordValid).toBe(true)
+        expect(validatePassword('Plcd$edq234d').isPasswordValid).toBe(true)
     })
 })
 
--- validatePassword.js
+++ validatePassword.js
@@ -1,20 +1,26 @@
 function validatePassword(password) {
+    let unmetConditionsArray = []
     let isPasswordValid;
     if (password == null) {
-        isPasswordValid = 'Invalid password: Passworld is null'
+        unmetConditionsArray.push('Invalid password: Password is null')
     } else if (password.length <= 9) {
-        isPasswordValid = 'Invalid password: Must be longer than eight characters'
+        unmetConditionsArray.push('Invalid password: Password is null')
     } else if (/(?=.*[A-Z])/gm.test(password) === false) {
-        isPasswordValid = 'Invalid password: Must have at least one uppercase letter'
+        unmetConditionsArray.push('Invalid password: Password is null')
     } else if (/(?=.*[a-z])/gm.test(password) === false) {
-        isPasswordValid = 'Invalid password: Must have at least one lowercase letter'
+        unmetConditionsArray.push('Invalid password: Password is null')
     } else if (/(?=.*[0-9])/gm.test(password) === false) {
         isPasswordValid = 'Invalid password: Must have at least one number'        
     } else {
         isPasswordValid = true
     }
 
-    return isPasswordValid
+    let returnObject = {
+        'unmetConditionsArray' : unmetConditionsArray,
+        'isPasswordValid' : isPasswordValid
+    }
+
+    return returnObject
 }
 
 
