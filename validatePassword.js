42ebc0e329283fe7ef939bb96fb695a047df838d
fal343 <98967311+fal343@users.noreply.github.com>
2022-02-08 11:11:38 +0000

    break else if into single if statements to construct error array. Start to work on condition Counter to fufill additional feature no.1

--- validatePassword.js
+++ validatePassword.js
@@ -1,17 +1,31 @@
 function validatePassword(password) {
     let unmetConditionsArray = []
     let isPasswordValid;
+    let conditionCounter = 5
+    
     if (password == null) {
         unmetConditionsArray.push('Invalid password: Password is null')
-    } else if (password.length <= 9) {
-        unmetConditionsArray.push('Invalid password: Password is null')
-    } else if (/(?=.*[A-Z])/gm.test(password) === false) {
-        unmetConditionsArray.push('Invalid password: Password is null')
-    } else if (/(?=.*[a-z])/gm.test(password) === false) {
-        unmetConditionsArray.push('Invalid password: Password is null')
-    } else if (/(?=.*[0-9])/gm.test(password) === false) {
-        isPasswordValid = 'Invalid password: Must have at least one number'        
-    } else {
+        conditionCounter -= 1
+    }
+    if (password.length <= 9) {
+        unmetConditionsArray.push('Invalid password: Must be longer than eight characters')
+        conditionCounter -= 1
+    }
+    if (/(?=.*[A-Z])/gm.test(password) === false) {
+        unmetConditionsArray.push('Invalid password: Must have at least one uppercase letter')
+        conditionCounter -= 1
+    }
+    if (/(?=.*[a-z])/gm.test(password) === false) {
+        unmetConditionsArray.push('Invalid password: Must have at least one lowercase letter')
+        conditionCounter -= 1
+    }
+    if (/(?=.*[0-9])/gm.test(password) === false) {
+        unmetConditionsArray.push('Invalid password: Must have at least one number')
+        conditionCounter -= 1
+    }
+
+
+    if (conditionCounter > 0) {
         isPasswordValid = true
     }
 
